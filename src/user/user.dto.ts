d86895e976bd07f259068b8555228c24a520785a
Quang Minh PHAM <quangminh1575@gmail.com>
2024-08-17 09:40:49 +0200

    update

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,14 +1,20 @@
+import { PartialType } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { UserRole } from './user.model';
-import { PartialType } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
+  @IsEmail()
   email: string;
 
+  @IsNotEmpty()
+  @IsString()
   password: string;
 
+  @IsNotEmpty()
   role: UserRole;
 }
 
